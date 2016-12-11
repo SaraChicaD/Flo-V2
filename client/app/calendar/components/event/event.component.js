d860c5a7afc27d4961d6089a5a3172e90fad7656
Holly Gibson <hollyglotapps@gmail.com>
2016-12-11 12:46:10 -0600

    Add moment-timezone

--- client/app/calendar/components/event/event.component.js
+++ client/app/calendar/components/event/event.component.js
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+// import { StorageService } from '../../../auth/services/storage/storage.service';
 import template from './event.template.html';
+import moment from 'moment-timezone';
 
 @Component({
   selector: 'event',
@@ -10,20 +12,20 @@ import template from './event.template.html';
 export class EventComponent implements OnInit {
 
   constructor(
-              // userService: UserService,
               router: Router) {
-
-    // this._userService = userService;
     this._router = router;
-    console.log('gapi', gapi)
+    this.now = false;
+    this.dates = {
+    	minDate: moment.tz().hour(12).startOf('h'), //12:00 User Timezone, today.
+    	maxDate: moment.tz().add(5, 'd').hour(12).startOf('h'), //12:00 User Timezone, in five days.
+    }
   }
 
   ngOnInit() {
-    this.testCalendar()
+    // this.testCalendar();
   }
 
   testCalendar() {
-
       gapi.client.load('calendar', 'v3', function() {
           let request = gapi.client.calendar.calendarList.list();
           request.execute((resp) => {
@@ -57,73 +59,60 @@ export class EventComponent implements OnInit {
   createEvent() {
 
     console.log('creating event');
-    
-  //   let event = {
-  //     'summary': 'Period #MyFlo',
-  //     'location': 'Wherever I am',
-  //     'description': 'bleeding all day',
-  //     'start': {
-  //       'dateTime': $scope.dates.minDate.format(),
-  //     },
-  //     'end': {
-  //       'dateTime': $scope.dates.maxDate.format(),
-  //     },
-  //     'reminders': {
-  //       'useDefault': false,
-  //       'overrides': [
-  //         {'method': 'email', 'minutes': 24 * 60},
-  //         {'method': 'popup', 'minutes': 10}
-  //       ]
-  //     }
-  //   };
-  //
-  //   gapi.client.load('calendar', 'v3', function() {
-  //
-	// 		var request = gapi.client.calendar.calendarList.list();
-  //
-	// 		request.execute(function(resp){
-	// 			// Check if Flo is present, else create it
-	// 			var flo = resp.items.filter(function(item) {
-	// 				return item.summary === 'Flo';
-	// 			})
-	// 			if (flo.length === 0) {
-	// 				gapi.client.calendar.calendars.insert({
-	// 					summary: 'Flo'
-	// 				}).execute();
-	// 			}
-	// 		});
-  //
-	// 		var request = gapi.client.calendar.calendarList.list();
-	// 		request.execute(function(resp){
-	// 			// Check if Flo is present, else create it
-	// 			var floCal = resp.items.filter(function(item) {
-	// 				return item.summary === 'Flo';
-	// 			});
-  //
-	// 			var floId = floCal[0].id;
-  //
-	// 			var request = gapi.client.calendar.events.insert({
-	// 				'calendarId': floId,
-	// 				'resource': event
-	// 			});
-	// 			console.log('request', request);
-  //
-	// 			request.execute(function(event) {
-	// 				$('.event').append('Event created: ' + event.htmlLink);
-  //
-	// 				console.log('event', event);
-	// 			});
-  //
-	// 			});
-  //   });
-  // }
-  //
-	// $scope.now = false;
-  //
-	// $scope.dates = {
-	// 	minDate: moment.tz().hour(12).startOf('h'), //12:00 User Timezone, today.
-	// 	maxDate: moment.tz().add(5, 'd').hour(12).startOf('h'), //12:00 User Timezone, in five days.
-	// };
+
+    let event = {
+      'summary': 'Period #MyFlo',
+      'location': 'Wherever I am',
+      'description': 'bleeding all day',
+      'start': {
+        'dateTime': this.dates.minDate.format(),
+      },
+      'end': {
+        'dateTime': this.dates.maxDate.format(),
+      },
+      'reminders': {
+        'useDefault': false,
+        'overrides': [
+          {'method': 'email', 'minutes': 24 * 60},
+          {'method': 'popup', 'minutes': 10}
+        ]
+      }
+    }
+
+    gapi.client.load('calendar', 'v3', () => {
+
+			let request = gapi.client.calendar.calendarList.list();
+			request.execute((resp) => {
+        console.log('resp', resp);
+				// Check if Flo is present, else create it
+				let floCal = resp.items.filter((item) => {
+					return item.summary === 'Flo';
+				});
+
+				let floId = floCal[0].id;
+
+				let request = gapi.client.calendar.events.insert({
+					'calendarId': floId,
+					'resource': event
+				});
+				console.log('request', request);
+
+				request.execute((event) => {
+					// $('.event').append('Event created: ' + event.htmlLink);
+
+					console.log('event', event);
+				});
+
+			});
+    });
+  }
+
+  onDateRangeChanged(event:any) {
+        console.log('onDateRangeChanged(): Begin date: ', event.beginDate, ' End date: ', event.endDate);
+        console.log('onDateRangeChanged(): Formatted: ', event.formatted);
+        console.log('onDateRangeChanged(): BeginEpoc timestamp: ', event.beginEpoc, ' - endEpoc timestamp: ', event.endEpoc);
+    }
+
 	// $scope.options = {
 	// 	view: 'date',
 	// 	format: 'lll',
@@ -167,5 +156,5 @@ export class EventComponent implements OnInit {
 	// 	if (values) {
 	// 		$scope.$broadcast('pickerUpdate', pickersToUpdate, values);
 	// 	}
-	}
+	// }
 }
